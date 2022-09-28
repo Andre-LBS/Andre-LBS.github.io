4e3a39b713ee1f59047244d752d8ce05bab77412
AndreSynZ <114581315+Andre-LBS@users.noreply.github.com>
2022-09-28 14:02:11 +0200

    Update: Particle & Cursor

--- script.js
+++ script.js
@@ -47,7 +47,7 @@ class Particle {
 		this.size = Math.random() * 20 + 1;
 		this.speedX = Math.random() * 3 - 1.5;
 		this.speedY = Math.random() * 3 - 1.5;
-		this.color = "rgba(255,255,255,255)";
+		this.color = "rgba(255,255,255,0.2)";
 	}
 	update() {
 		this.x += this.speedX;
