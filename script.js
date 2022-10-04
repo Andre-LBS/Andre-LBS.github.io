853428cca5c27db3ba925c01848460b21a685c31
AndreSynZ <114581315+Andre-LBS@users.noreply.github.com>
2022-10-04 09:45:44 +0200

    jscmt

--- script.js
+++ script.js
@@ -114,21 +114,4 @@ animate();
 
 
 
-// log it
-function swRegistration() {
-	const heart = ["font-size: 20px", "padding: 12px", "margin: 4px 0 4px 4px", "color: rgba(238,58,136,1)"].join(";");
-	if ("serviceWorker" in navigator) {
-		navigator.serviceWorker
-			.register("/mousetrail-space/sw.js", {
-				scope: "/mousetrail-space/",
-			})
-			.then(function (registration) {
-				console.log("%c❤️", heart);
-			})
-			.catch(function (err) {
-				console.log(err);
-			});
-	}
-}
 
-swRegistration();
